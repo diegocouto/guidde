286be9099cc71c7bc2eda355305bed759a3919c1
Diego Couto <diego@couto.dev>
2022-01-28 10:53:40 +0100

    Rename cache dir

--- utils/libs/cache.ts
+++ utils/libs/cache.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 import { getArticle, getArticlesList } from '../datasource';
 
-const CACHE_PATH = 'cache';
+const CACHE_PATH = '_cache';
 const CACHE_FILE = 'data.json';
 
 export interface CacheEntry {
